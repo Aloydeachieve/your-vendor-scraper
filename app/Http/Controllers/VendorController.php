051eb98f795c7d5b8b098f7e3a287d1fc8b08285
Aloydeachieve <donaloy777@gmail.com>
2026-02-24 21:42:52 +0100

    cleaner vendorcontroller

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -1,19 +1,23 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 
-class VendorController extends Controller {
-    public function scrape(Request $request) {
-        $query = $request->query || 'phones/lagos';
-        $pages = $request->pages ?? 1;
-        
-        $process = new Process(['node', app_path('scraper.js'), "https://jiji.ng/{$query}", $pages]);
+class VendorController extends Controller
+{
+    public function scrape(Request $request)
+    {
+        $query = $request->query('url') ?? 'phones/lagos';
+        $pages = $request->query('pages') ?? 1;
+
+        $process = new Process(['node', base_path('app/scraper.js'), "https://jiji.ng/{$query}", $pages]);
         $process->start();
         $process->wait();
-        
+
         $vendors = json_decode($process->getOutput(), true) ?: [];
-        
+
         return response()->json([
             'count' => count($vendors),
             'data' => $vendors
